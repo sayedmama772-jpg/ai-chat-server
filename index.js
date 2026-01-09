6ffb95414776f10d1ef733e2b4e16f8528511846
sayedmama772-jpg <sayedmama772@gmail.com>
2026-01-09 14:46:35 +0600

    Update index.js

--- index.js
+++ index.js
@@ -87,3 +87,20 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// ======================
+// Messenger Webhook Verify
+// ======================
+app.get("/webhook", (req, res) => {
+  const VERIFY_TOKEN = "my_verify_token"; // Meta তে যেটা লিখছো, হুবহু একই
+
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("Webhook verified successfully");
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
